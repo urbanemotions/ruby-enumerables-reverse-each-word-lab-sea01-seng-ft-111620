93c95ebd1938b7656139b2e0c5c1af415f64147d
Sin Wildy <sw.wildy@gmail.com>
2020-11-06 18:13:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   sentence_reverse = []
   sentence.split(" ").each { |word| 
   sentence_reverse << word.reverse }
-  sentence_reverse == sentence_reverse.join(' ')
+  sentence_reverse
   #sentence.split(" ").collect { |word| word.reverse }.join(' ')
 end
