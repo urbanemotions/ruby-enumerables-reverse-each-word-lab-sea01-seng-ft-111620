6dcecd8fad2528d8311ad7304c5d4a7d1d34c5a8
Sin Wildy <sw.wildy@gmail.com>
2020-11-06 18:16:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(sentence)
-  sentence_reverse = []
-  sentence.split(" ").each { |word| 
-  sentence_reverse << word.reverse }
-  sentence_reverse.join(' ')
-  #sentence.split(" ").collect { |word| word.reverse }.join(' ')
+  #each failed because does not use collect 
+  #sentence_reverse = []
+  #sentence.split(" ").each { |word| 
+  #sentence_reverse << word.reverse }
+  #sentence_reverse.join(' ')
+  
+  #use collect 
+  sentence.split(" ").collect { |word| word.reverse }.join(' ')
 end
